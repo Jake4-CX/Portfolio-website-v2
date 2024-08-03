6fce85d271a8d2751658ea85ca5697512456fef9
= <jake44264@hotmail.co.uk>
2024-08-03 19:39:28 +0100

    feat: display technologies on dashboard

---
+++ src/components/dashboard/technologies/allTechnologies.tsx
@@ -0,0 +1,143 @@
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { ColumnDef, SortingState, flexRender, getCoreRowModel, getPaginationRowModel, getSortedRowModel, useReactTable } from "@tanstack/react-table"
+import { RefreshCw } from "lucide-react";
+import { useState } from "react";
+
+interface TechnologiesTableComponentProps {
+  technologies: Technology[] | undefined
+}
+
+export const columns: ColumnDef<Technology>[] = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+  },
+  {
+    accessorKey: 'technologyName',
+    header: 'Technology Name',
+  },
+  {
+    accessorKey: 'technologyImage',
+    header: 'Technology Image',
+    cell: ({ row }) => (
+      <div className="flex items-center space-x-2">
+        <img src={row.original.technologyImage ?? ""} alt={row.original.technologyName} className="w-8 h-8 rounded-full" />
+      </div>
+    )
+  },
+  {
+    accessorKey: 'technologyType',
+    header: 'Technology Type',
+    cell: ({ row }) => (
+      <span className="capitalize">{row.original.technologyType}</span>
+    )
+  },
+  {
+    header: 'Actions',
+    cell: () => (
+      <div className="flex items-center space-x-2">
+        <Button variant="outline" size="sm">Edit</Button>
+        <Button variant="outline" size="sm">Delete</Button>
+      </div>
+    )
+  }
+]
+
+const AllTechnologiesComponent: React.FC<TechnologiesTableComponentProps> = ({ technologies }) => {
+
+  const [sorting, setSorting] = useState<SortingState>([
+    {
+      id: 'id',
+      desc: true
+    }
+  ])
+
+  const table = useReactTable({
+    data: technologies || [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    }
+  })
+
+  return (
+    <>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                  </TableHead>
+                )
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+
+        <TableBody>
+          {technologies !== undefined || table.getRowModel().rows.length ? (
+            table.getRowModel().rows.length !== 0 ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="text-center">
+                  No technologies found
+                </TableCell>
+              </TableRow>
+            )
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="text-center">
+                <RefreshCw className="animate-spin w-6 h-6" />
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <div className="flex items-center justify-end space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default AllTechnologiesComponent;
--- src/views/dashboard.tsx
+++ src/views/dashboard.tsx
@@ -1,3 +1,4 @@
+import AllTechnologiesComponent from "@/components/dashboard/technologies/allTechnologies";
 import {
   Card,
   CardContent,
@@ -5,27 +6,47 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { getTechnologies } from "@/api/technologies";
 
 
 import DefaultLayout from "@/layouts/defaultLayout";
 import { useAppSelector } from "@/redux/store";
+import { useQuery } from "@tanstack/react-query";
 
 const DashboardPage: React.FC = () => {
 
   const userRedux = useAppSelector((state) => state.userReduser.value);
   const userRole = userRedux.userData?.userRole;
 
+  const getTechnologiesQuery = useQuery({
+    queryKey: ["technologies"],
+    staleTime: 1000 * 60 * 5,
+    queryFn: async () => {
+
+      const technologiesResponse = await getTechnologies();
+
+      const data = (technologiesResponse.data).technologies as Technology[];
+
+      return data;
+    }
+  })
+
   return (
     <DefaultLayout>
       <Card className="w-full h-[32rem] mt-[4%]">
         <CardHeader>
-          <CardTitle>LWS Dashboard</CardTitle>
-        </CardHeader>
-        <CardContent>
+          <CardTitle>Portfolio Dashboard</CardTitle>
           <CardDescription>
             Welcome back, {userRedux.userData?.userEmail}!
             You are currently logged in as a {userRole} user.
           </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {/* Projects */}
+
+          {/* Technologies */}
+          <AllTechnologiesComponent technologies={getTechnologiesQuery.data} />
+          
         </CardContent>
       </Card>
     </DefaultLayout>
